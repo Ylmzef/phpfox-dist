bece0910b1484214331699099ba4d5028ba6e175
Raymond Benc <r@phpfox.com>
2015-05-20 12:21:25 +0200

    Dist: 1432117285

--- PF.Base/vendor/autoload.php
+++ PF.Base/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit6071ebc1fbb0f11a7166f0ef42f13819::getLoader();
+return ComposerAutoloaderInit962fa95f01416ee173992161772df28d::getLoader();
